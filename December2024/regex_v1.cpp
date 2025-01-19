14b0e1511c233d4c00b5f0468208ab7fbb15293a
Volodymyr Hrudnytskyi <lintalve@gmail.com>
2025-01-19 18:37:01 +0100

    included all sources files from quickPractice and removed deleted

--- December2024/regex_v1.cpp
+++ December2024/regex_v1.cpp
@@ -1,3 +1,6 @@
+/*
+ The provided code will not work as intended because std::regex_match checks if the entire input string matches the given regex pattern. In your case, the pattern R"(v)" only matches the character v, while the entire input string is "v 0.1 0.2 0.3". Since the entire string doesn't match, std::regex_match will fail, and matches will remain empty.
+ */
 #include <iostream>
 #include <regex>
 
